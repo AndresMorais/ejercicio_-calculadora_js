d1532325f427a7a6f342ffa6493066634d815bdc
Andres Morais <andres.decmorais@gmail.com>
2020-10-15 20:53:07 -0300

    funcionalidad: numero
    ingresa los numeros en la "pantalla" de la calculadora.

--- funciones.js
+++ funciones.js
@@ -1,4 +1,16 @@
-function init(){
-    alert ('hola mundo')
+{/* <script src="text/javascript">
+    $(document).ready(function(){
+        crear_cadena('#uno',1);
+    });
 
-}
+</script>
+
+<script type="text/javascript">
+ function crear_cadena(control,valor){
+     $('#' + control).click(function() {
+         var valor_actual=$('#resultado').val(); 
+         cadena= valor_actual + valor;  
+         $('#resultado').var(cadena);
+     });
+     
+ } */}
